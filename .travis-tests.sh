f29a76d7b0ee363580867c987d27aa76ec61b3cc
Jay Hesselberth <jay.hesselberth@gmail.com>
2016-01-09 07:19:55 -0700

    update test

--- .travis-tests.sh
+++ .travis-tests.sh
@@ -3,10 +3,10 @@
 test -e ssshtest || wget -q https://raw.githubusercontent.com/ryanlayer/ssshtest/master/ssshtest
 source ssshtest
 
-set -o nounset
+set -o nounset -x
 
 run check_dir ls projects/
-assert_no_stdout
+assert_no_sterr
 
 run check_bed file result.bed.gz
 assert_in_stdout compressed
